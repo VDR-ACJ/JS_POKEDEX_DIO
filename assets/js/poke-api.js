a3ef84bb2cfa3032c29ee454bb755f9594f37d36
valdeir <valdeir_014@hotmail.com>
2023-01-07 22:33:06 -0300

    Consumindo detalhe do pokemon na pokeapi

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -2,11 +2,28 @@
 
 const pokeApi = {}
 
-pokeApi.getPokemons = (offset = 0, limit = 10) =>{
+pokeApi.getPokemonsDetail = (pokemon) =>{
+    //busca os detalhes pela url vinda no resultado da primeira consulta
+    return fetch(pokemon.url).then((response) => response.json())
+}
+
+pokeApi.getPokemons = (offset = 0, limit = 5) =>{
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
     //fetch retorna uma promisse uma promessa de resposta por padrao fetch usa get
 return fetch(url)
-.then((response) => response.json())
-.then((jsonBody) => jsonBody.results)
-.catch((error)=> console.error(error))
-}
+    .then((response) => response.json())
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemons)=> pokemons.map(pokeApi.getPokemonsDetail))
+    .then((detailRequests) => Promise.all(detailRequests))
+    .then((pokemonsDetails)=>pokemonsDetails)
+    .catch((error)=> console.error(error))
+    }
+
+//https://pokeapi.co/api/v2/pokemon/1/
+//lista de promessas
+/*Promise.all(
+    fetch('https://pokeapi.co/api/v2/pokemon/1/'),
+    fetch('https://pokeapi.co/api/v2/pokemon/1/')
+).then((results) =>{
+    console.log("results");
+})*/
